cc66adc8d7421e51370025b21a882f01b684bb9e
Soufiane-2005 <soufianesbaa2005@gmail.com>
2026-08-16 13:55:37 +0100

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+
+        int n = nums.size();
+        int min_num = INT_MAX;
+        for(int i = 0 ; i<n ;i++){
+            min_num = min(min_num,nums[i]);
+        }
+
+        return min_num;
+        
+    }
+};
