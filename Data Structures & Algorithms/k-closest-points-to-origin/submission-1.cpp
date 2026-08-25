55d99d9fc4181ab576c8524edb3a0b5bfeb383ee
Soufiane-2005 <soufianesbaa2005@gmail.com>
2026-08-25 21:09:30 +0100

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+
+        priority_queue<pair<double,int>,vector<pair<double,int>>, greater<pair<double,int>>> pq;
+        int c = 0;
+        for(vector<int> point : points){
+            pair<double,int> p;
+            p.first = distance(point);
+            p.second = c;
+            c++;
+            pq.push(p);
+        }
+        vector<vector<int>> res;
+        while(k!=0){
+            pair<double,int> p = pq.top();
+            res.push_back(points[p.second]);
+            pq.pop();
+            k--;
+        }
+
+        return res;
+        
+    }
+
+    double distance(vector<int> coord){
+        int x = coord[0];
+        int y = coord[1];
+        return sqrt(pow(x,2)+pow(y,2));
+    }
+};
