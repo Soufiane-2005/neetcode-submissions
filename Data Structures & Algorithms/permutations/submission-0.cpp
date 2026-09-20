2d576e2e1f30ebc1c734a902b8bd3b1c51565316
Soufiane-2005 <soufianesbaa2005@gmail.com>
2026-09-20 10:53:47 +0100

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>> result;
+        vector<int> path;
+        int n = nums.size();
+        unordered_set<int> chosen;
+        backtracking(path,nums,chosen,n,result);
+        return result;
+        
+    }
+
+    void backtracking(vector<int>& path, vector<int>& nums,unordered_set<int> chosen, int n, vector<vector<int>>& result){
+
+        if(path.size()==n){
+            result.push_back(path);
+            return;
+        }
+
+        for(int i=0 ; i<n ; i++){
+            if(chosen.contains(nums[i]))continue;
+            path.push_back(nums[i]);
+            chosen.insert(nums[i]);
+            backtracking(path,nums,chosen,n,result);
+            chosen.erase(nums[i]);
+            path.pop_back();
+        }
+    }
+};
